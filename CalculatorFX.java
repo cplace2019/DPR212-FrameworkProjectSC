02210c3208a475b3dfffd2bcb36633f5721626bd
cplace2019 <46765978+cplace2019@users.noreply.github.com>
2019-04-27 09:02:15 -0400

    Update CalculatorFX.java

--- CalculatorFX.java
+++ CalculatorFX.java
@@ -1,8 +1,6 @@
 /**
 Carmen Renwick, DPR212(Dsa), CalculatorFXv3
 The CalculatorFX program allows users to perform basic calculation functions such as addition, substration, multiplication and division.  It provides: digit buttons,  operation buttons, a clear button and a display to show results. 
-
-Contributions: CalculatorFXv0, CalculatorFXv1, CalculatorFXv2, CalculatorFXv3 
  */
 
 package calculatorfx;
